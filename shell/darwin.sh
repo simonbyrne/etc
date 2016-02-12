0a540ce18339d6c4c6c221376bebedd04934ba5d
Simon Byrne <simonbyrne@gmail.com>
2016-02-12 14:36:03 +0000

    add notify command on os x

--- shell/darwin.sh
+++ shell/darwin.sh
@@ -5,8 +5,23 @@ fi
 
 # OS X only
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
 alias sleepnow="pmset sleepnow"
+alias reboot="shutdown -r now"
 
 # see https://coolaj86.com/articles/how-to-control-os-x-system-volume-with-applescript/
 alias mute="osascript -e 'set volume with output muted'"
 alias unmute="osascript -e 'set volume without output muted'"
+
+
+
+function notify {
+    WID=`osascript -e 'tell app "Terminal" to id of first window'`
+    $*
+    osascript <<EOC
+tell app "Terminal"
+    set frontmost of window id $WID to true
+    activate
+end tell
+EOC
+}
