e9a82708d7439f97843a076c715b03ecef2ef5fd
Simon Byrne <simonbyrne@gmail.com>
2018-06-20 13:10:46 -0700

    update shell

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -11,11 +11,11 @@ case "$TERM" in
 	if [ -n "$STY" ]; then
 	    PS1_SCREEN="[s: $STY]";
         elif [ -n "$TMUX" ]; then
-	    PS1_SCREEN="[t: $TMUX]";            
+	    PS1_SCREEN="[t: $TMUX]";
 	else
 	    PS1_SCREEN='─────────────────────────────────';
 	fi
-	
+
 	if type __git_ps1 >/dev/null 2>&1; then
 	    PS1_GIT='$(__git_ps1 "(\[\e[1;30m\]%s\[\e[0;30m\])")';
             GIT_PS1_SHOWDIRTYSTATE=1
@@ -30,10 +30,28 @@ case "$TERM" in
 	unset PS1_HOST PS1_GIT PS1_SCREEN
 	;;
     *)
-	# prompt format
-	PS1='┌─────────────────────────────────────────────────────────────────────\n│ \u@\h : \w \n├ '
-	PS2='├ '
+	if [ -n "$STY" ]; then
+	    PS1_SCREEN="[s: $STY]";
+        elif [ -n "$TMUX" ]; then
+	    PS1_SCREEN="[t: $TMUX]";
+	else
+	    PS1_SCREEN='─────────────────────────────────';
+	fi
+
+	if type __git_ps1 >/dev/null 2>&1; then
+	    PS1_GIT='$(__git_ps1 "(%s)")';
+            GIT_PS1_SHOWDIRTYSTATE=1
+            GIT_PS1_SHOWSTASHSTATE=1
+            # GIT_PS1_SHOWUNTRACKEDFILES=1
+            GIT_PS1_SHOWUPSTREAM="auto"
+	else
+	    PS1_GIT='';
+	fi
+	PS1='┌─[\#]──[\d]──[\t]──'"$PS1_SCREEN"'─────\n│ \u@\h:\w '"$PS1_GIT"'\n├ '
+	PS2='├'
+	unset PS1_GIT PS1_SCREEN
 	;;
+
 esac
 # set maxdepth
 PROMPT_DIRTRIM=4
